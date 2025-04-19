ca1acda6a166340c52b0af0741e4981b3a287ba0
prlgadminuser <liam.heizmann@gmail.com>
2025-04-19 03:33:10 +0200

    Update gamemodes.js

--- gameconfig/gamemodes.js
+++ gameconfig/gamemodes.js
@@ -38,7 +38,7 @@ const gamemodeconfig = {
     3: {
       can_hit_dummies: true,
       can_hit_players: false,
-      maxplayers: 1,
+      maxplayers: 2,
       respawns_allowed: 1,
       playerhealth: 50,
       playerspeed: 1.7,
@@ -49,7 +49,7 @@ const gamemodeconfig = {
       seasoncoinsreward: [0],
       show_timer: true,
       custom_map: 3,
-      teamsize: 1,
+      teamsize: 2,
       //health_autodamage: true,
     },
     4: {
