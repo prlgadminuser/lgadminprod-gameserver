ac178df8bdaca5063cd3552784f39b861ae50bb2
prlgadminuser <liam.heizmann@gmail.com>
2025-08-06 21:10:53 +0200

    Update idbconfig.js

--- idbconfig.js
+++ idbconfig.js
@@ -1,11 +1,11 @@
 
-
-
-const { MONGO_URI, TOKEN_KEY, DISCORDWEBHOOK } = require("./ENV")
+const { MONGO_URI, TOKEN_KEY, DISCORDWEBHOOK, REDIS_KEY } = require("./ENV")
 
 const lgconnecturi = process.env.MONGO_URI || MONGO_URI
 const tokenkey = process.env.TOKEN_KEY || TOKEN_KEY
 const webhookURL = process.env.DISCORDWEBHOOK || DISCORDWEBHOOK
+const rediskey = process.env.REDIS_KEY || REDIS_KEY
+
 
 
 const uri = lgconnecturi
@@ -13,4 +13,5 @@ const uri = lgconnecturi
 module.exports = {
    uri,
    tokenkey,
+   rediskey,
 }
