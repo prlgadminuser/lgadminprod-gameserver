4d36982761eb10af8cc0f196d3aa32b65aa266bc
prlgadminuser <liam.heizmann@gmail.com>
2025-08-07 03:55:39 +0200

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const SERVER_INSTANCE_ID = 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx'.replace(/[xy]/
 
 const USER_SESSION_MAP_KEY = 'user_to_server_map'; // Redis Hash key for user -> server mapping
 const SERVER_HEARTBEAT_PREFIX = 'server_heartbeat:'; // Prefix for server heartbeat keys
-const multiplier = 10
+const multiplier = 30
 const HEARTBEAT_INTERVAL_MS = 10000 * multiplier; // Send heartbeat every 5 seconds
 const HEARTBEAT_TTL_MS = 30000 * multiplier / 1000;   // Heartbeat expires after 15 seconds (should be > interval)
 const CLEANUP_INTERVAL_MS = 60000 * multiplier;  // Run stale session cleanup every 30 seconds (must be > HEARTBEAT_TTL_SECONDS)
