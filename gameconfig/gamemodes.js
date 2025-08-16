c4c68c624abde44066d2b5b85b4c34e232287f11
prlgadminuser <liam.heizmann@gmail.com>
2025-08-16 13:10:53 +0200

    Update gamemodes.js

--- gameconfig/gamemodes.js
+++ gameconfig/gamemodes.js
@@ -57,7 +57,7 @@ const gamemodeconfig = {
     can_hit_dummies: false,
     can_hit_players: true,
 
-    maxplayers: 4,
+    maxplayers: 10,
     teamsize: 1,
     respawns_allowed: 0,
 
@@ -188,3 +188,4 @@ module.exports = {
   gamemodeconfig: new Map(Object.entries(gamemodeconfig)),
   allowed_gamemodes,
 };
+
