cf1fb562d0e54a4a73a61188874fca276e7b4fec
prlgadminuser <liam.heizmann@gmail.com>
2025-08-28 07:13:34 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -472,6 +472,12 @@ wss.on("connection", async (ws, req) => {
       }
     });
 
+    ws.on("pong", () => {
+    //const player = joinResult.room.players.get(joinResult.playerId);
+    const now = Date.now();
+    player.ping_ms = now - player.pingstart_ms;
+    });
+
     ws.on("close", async () => {
       // Marked async for Redis operations
 
