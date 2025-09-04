95321a7755b2ea0544f050b67e7d305d90249a3c
prlgadminuser <liam.heizmann@gmail.com>
2025-09-04 04:23:21 +0200

    Add files via upload

--- src/Battle/NetworkLogic/Packets.js
+++ src/Battle/NetworkLogic/Packets.js
@@ -1,7 +1,7 @@
 const { arraysEqual } = require("@main/modules");
 const { compressMessage } = require("./compress");
 const { playerchunkrenderer } = require("../PlayerLogic/playerchunks");
-const { handlePlayerMoveIntervalAll } = require("./handleMessage");
+const { handlePlayerMoveIntervalAll } = require("@Battle/NetworkLogic/HandleMessage");
 const { handleSpectatorMode } = require("../PlayerLogic/spectating");
 const { HandleAfflictions } = require("../WeaponLogic/bullets-effects");
 
