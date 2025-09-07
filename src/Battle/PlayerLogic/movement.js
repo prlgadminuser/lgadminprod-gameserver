d969d8e76ad9d921c7d3341b5758c87596c621ce
prlgadminuser <liam.heizmann@gmail.com>
2025-09-07 10:27:37 +0200

    Update movement.js

--- src/Battle/PlayerLogic/movement.js
+++ src/Battle/PlayerLogic/movement.js
@@ -48,10 +48,10 @@ function handleMovement(player, room) {
     newY = Math.max(-mapHeight, Math.min(mapHeight, newY));
 
     // Apply updated position
-  //  player.x = Math.round(newX * 100) / 100;
-   //player.y = Math.round(newY * 100) / 100;
-    player.x = newX;
-    player.y = newY;
+   player.x = Math.round(newX * 100) / 100;
+   player.y = Math.round(newY * 100) / 100;
+  //  player.x = newX;
+  //  player.y = newY;
 
 
     if (player._gridKey) room.realtimegrid.updateObject(player, player.x, player.y);
@@ -165,4 +165,5 @@ module.exports = {
   playerhitbox,
 
 }
+
 
