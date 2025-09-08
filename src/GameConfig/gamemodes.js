2a0cf3ed0153106d694c50245b7011156feec39b
prlgadminuser <liam.heizmann@gmail.com>
2025-09-08 23:39:44 +0200

    Update gamemodes.js

--- src/GameConfig/gamemodes.js
+++ src/GameConfig/gamemodes.js
@@ -62,7 +62,7 @@ const gamemodeconfig = {
     respawns_allowed: 0,
 
     playerhealth: 100,
-    playerspeed: 2,
+    playerspeed: 2.4,
 
     modifiers: new Set(["UseZone", "AutoHealthRestore"]),
 
@@ -191,6 +191,7 @@ module.exports = {
   gamemodeconfig: new Map(Object.entries(gamemodeconfig)),
   allowed_gamemodes,
 };
+
 
 
 
