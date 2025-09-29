381311f4199acaa47adc401c055c9dc49b1c8c6f
prlgadminuser <liam.heizmann@gmail.com>
2025-09-29 16:25:44 +0200

    Update Packets.js

--- src/Battle/NetworkLogic/Packets.js
+++ src/Battle/NetworkLogic/Packets.js
@@ -259,11 +259,16 @@ function prepareRoomMessages(room) {
     const serialized = SerializePlayerData(p);
 
     // Use hash instead of full array equality for efficiency
-  const hash = serialized.join(",");
+    const hash = serialized.join(",");
+
    p.dirty = hash !== p._lastSerializedHash;
    p._lastSerializedHash = hash;
 
-   if (p.dirty) playerData.set(p.id, serialized);
+   if (p.dirty) {
+    playerData.set(p.id, serialized);
+} else {
+    playerData.delete(p.id); // remove old data if player is not dirty
+}
   }
 
   // ONE PASS: build messages
@@ -289,6 +294,7 @@ function prepareRoomMessages(room) {
       for (const nearbyId of p.nearbyplayersids) {
         const data = playerData.get(nearbyId);
         if (data || !p.nearbyplayersidslast.includes(nearbyId)) filteredPlayers.push(data); // if data is dirty or playerid is new from last tick then sent
+
       }
 
       if (filteredPlayers.length > 0)  p.latestnozeropd = filteredPlayers;
