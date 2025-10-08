606b5d52dd33b79c51f38c27031aca929a4466e0
prlgadminuser <liam.heizmann@gmail.com>
2025-10-08 02:29:04 +0200

    Update CreateRoom.js

--- src/RoomHandler/CreateRoom.js
+++ src/RoomHandler/CreateRoom.js
@@ -259,7 +259,7 @@ class Room {
     this.startGameLoop(game_tick_rate);
   }
 
-   startGameLoop2(game_tick_rate) {
+   startGameLoop(game_tick_rate) {
   const idealDt = game_tick_rate; // e.g., 25 ms for 40 Hz
   this._tickTimes = [];
 
@@ -294,7 +294,7 @@ class Room {
 
 
     // Game tick loop
-  startGameLoop(game_tick_rate) {
+  startGameLoop2(game_tick_rate) {
   let nextTick = performance.now();
   const tickRateMs = game_tick_rate;
 
@@ -336,3 +336,4 @@ class Room {
 
 
 module.exports = { Room };
+
