ab461b72da052c5efa0d6b66e4478748319693c6
prlgadminuser <liam.heizmann@gmail.com>
2025-11-14 23:35:25 +0100

    Add files via upload

---
+++ src/objects/bullets-effects.js
@@ -0,0 +1,41 @@
+
+
+
+function HandleAfflictions(room) {
+  const now = Date.now();
+
+  // Iterate backward to allow removal
+  for (let i = room.activeAfflictions.length - 1; i >= 0; i--) {
+    const aff = room.activeAfflictions[i];
+
+    // Remove if expired
+    if (now >= aff.expires) {
+      room.activeAfflictions.splice(i, 1);
+      continue;
+    }
+
+    // Skip until next tick
+    if (now < aff.nextTick) continue;
+
+    // Apply effect
+    if (aff.target_type === "dummy") {
+      if (!room.dummies[aff.dummykey]) {
+        room.activeAfflictions.splice(i, 1);
+        continue;
+      }
+       aff.shootingPlayer.HandleSelfBulletsOtherPlayerCollision(aff.target, aff.damage, aff.gunid)
+    } else if (aff.target_type === "player") {
+      if (!aff.target.alive) {
+        room.activeAfflictions.splice(i, 1);
+        continue;
+      }
+      aff.shootingPlayer.HandleSelfBulletsOtherPlayerCollision(aff.target, aff.damage, aff.gunid)
+    }
+
+    // Schedule next tick
+    aff.nextTick += aff.speed;
+  }
+}
+
+
+module.exports = { HandleAfflictions }
---
+++ src/utils/game.js
@@ -0,0 +1,34 @@
+
+
+module.exports = {
+  
+  validDirections: [-90, 0, 180, -180, 90, 45, 135, -135, -45],
+
+  isValidDirection(direction) {
+    const numericDirection = parseFloat(direction);
+    return (
+      !isNaN(numericDirection) &&
+      this.validDirections.includes(numericDirection)
+    );
+  },
+
+  encodePosition(num) {
+    return Math.round(num * 100); // keep 2 decimals
+  },
+
+  createHitmarker(target, shooter, damage) {
+    shooter.hitmarkers.push([
+      Math.round(target.x),
+      Math.round(target.y),
+      damage,
+    ]);
+  },
+
+  AddNewUnseenObject(room, obj) {
+  room.grid.addObject(obj);
+},
+
+//   const obj = { type: "spray", x: player.x, y: player.y }
+//    PlaceNewObject(this.room, obj)
+
+};
