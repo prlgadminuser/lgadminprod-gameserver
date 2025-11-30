ca5a35987df71f896cf3a51b899863930d29c941
prlgadminuser <liam.heizmann@gmail.com>
2025-11-30 20:37:55 +0100

    Update zone.js

--- src/modifiers/zone.js
+++ src/modifiers/zone.js
@@ -145,6 +145,9 @@ function UseZone(room) {
     { targetSize: room.mapHeight * 2, waitTime: 0, shrinkTime: 24000, damage: 2 },
     { targetSize: room.mapHeight * 1.3, waitTime: 20000, shrinkTime: 50000, damage: 5 },
     { targetSize: room.mapHeight * 0.6, waitTime: 20000, shrinkTime: 50000, damage: 8 },
+    { targetSize: room.mapHeight * 0.4, waitTime: 20000, shrinkTime: 50000, damage: 8 },
+    { targetSize: room.mapHeight * 0.2, waitTime: 20000, shrinkTime: 50000, damage: 8 },
+    { targetSize: room.mapHeight * 0.1, waitTime: 20000, shrinkTime: 50000, damage: 8 },
     { targetSize: 0, waitTime: 20000, shrinkTime: 50000, damage: 10 }
   ];
 
@@ -187,3 +190,4 @@ module.exports = {
   UseZone,
 
 }
+
