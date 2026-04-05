337d593d7f06ba206a2fd263279a280998aecae0
prlgadminuser <liam.heizmann@gmail.com>
2026-04-06 00:53:00 +0200

    Add files via upload

--- src/objects/weapons/bullet.js
+++ src/objects/weapons/bullet.js
@@ -1,13 +1,11 @@
 "use strict";
 
 const { gunsconfig } = require("../../config/guns");
-const {
-  AddNewUnseenObject,
-  isPositionOutsideMapBounds,
-} = require("../../utils/game");
-const { GlobalRoomConfig, GlobalServerConfig } = require("../../config/server");
+const { AddNewUnseenObject, isPositionOutsideMapBounds } = require("../../utils/game");
+const { GlobalRoomConfig } = require("../../config/server");
 const { sweptSATRectVsRect, Vec2 } = require("../../utils/bulletcollision");
 const { PoisonDamageHandler } = require("./weapon-effects");
+
 /* =========================
    BULLET
 ========================= */
@@ -18,31 +16,31 @@ class Bullet {
     this.prevPosition = this.position;
     this.spawnTime = Date.now();
     this.alive = true;
-    this.updatesTick = 1000;
-    this.directionChange = data.directionChange || null;
-    this.updates_per_tick = data.updates_per_tick;
+    this.updateTicks = 0;
+    this.lifeTicks = 0;
     this.effect = 1;
-    //this.effect = 1
+    this.new = true;
   }
 
   applyDirectionChange() {
     if (!this.directionChange) return;
-    const dc = this.directionChange;
-    if (dc.type === 1)
-      this.direction += Math.sin(this.lifeTicks * dc.frequency) * dc.amplitude;
-    if (dc.type === 2)
-      ((this.direction +=
-        dc.turnRate *
-        (GlobalRoomConfig.ticks_per_second / this.updates_per_tick)),
-        (this.dirVec = Vec2.fromAngle(this.direction - 90)));
+    const { type, frequency, amplitude, turnRate } = this.directionChange;
+    this.lifeTicks++;
+
+    if (type === 1) this.direction += Math.sin(this.lifeTicks * frequency) * amplitude;
+    else if (type === 2) this.direction += turnRate * (GlobalRoomConfig.ticks_per_second / this.updates_per_tick);
+
+    this.dirVec = Vec2.fromAngle(this.direction - 90);
   }
 
   nextPosition() {
     return this.position.add(this.dirVec.scale(this.speed));
   }
+
   kill() {
     this.alive = false;
   }
+
   isExpired() {
     return !this.alive || Date.now() > this.maxTime;
   }
@@ -69,214 +67,105 @@ class BulletManager {
     const id = this.generateBulletId();
     const baseDir = Vec2.fromAngle(bulletData.angle);
     const perpDir = Vec2.fromAngle(bulletData.angle - 90);
-    const initialPosition = new Vec2(player.position.x, player.position.y)
-      .add(baseDir.scale(bulletData.offset))
+    const position = new Vec2(player.position.x, player.position.y)
+      .add(baseDir.scale(bulletData.offset || 0))
       .add(perpDir.scale(25));
 
-    const bullet = new Bullet({
-      id,
-      position: initialPosition,
-      startPosition: initialPosition,
-      direction: bulletData.angle,
-      ...bulletData,
-      maxTime: bulletData.maxTime,
-      gunId: bulletData.gunId,
-      owner: player,
-      x: initialPosition.x,
-      y: initialPosition.y,
-      objectType: "bullet",
-      updateTicks: 0,
-      effect: 1,
-    });
-
-
+    const bullet = new Bullet({ id, position, startPosition: position, direction: bulletData.angle, owner: player, objectType: "bullet", ...bulletData });
     this.bullets.set(id, bullet);
     this.room.grid.addObject(bullet);
     return bullet;
   }
 
   update() {
-    // Handle Bullet Effects
     PoisonDamageHandler(this.room);
-
     this.processScheduledBullets();
+
     const toRemove = [];
 
     for (const [id, bullet] of this.bullets.entries()) {
-      if (!bullet || !bullet.alive || bullet.isExpired()) {
-        toRemove.push(id);
-        continue;
-      }
+      if (!bullet.alive || bullet.isExpired()) { toRemove.push(id); continue; }
+
       bullet.updateTicks++;
-      if (
-        bullet.updateTicks >
-        GlobalRoomConfig.ticks_per_second / bullet.updates_per_tick - 1
-      ) {
-        bullet.updateTicks = 0;
-        if (this.directionChange) this.directionChange.lifeTicks++;
-        bullet.applyDirectionChange();
+      if (bullet.updateTicks < GlobalRoomConfig.ticks_per_second / bullet.updates_per_tick) continue;
 
-        const prevPos = bullet.position;
-        const nextPos = bullet.nextPosition();
-        this.room.grid.updateObject(bullet, nextPos);
-        if (isPositionOutsideMapBounds(this.room, prevPos)) {
-          toRemove.push(id);
-          continue;
-        }
+      bullet.updateTicks = 0;
+      bullet.applyDirectionChange();
 
-        const halfW = bullet.width;
-        const halfH = bullet.height;
-        const minX = Math.min(prevPos.x, nextPos.x) - halfW;
-        const maxX = Math.max(prevPos.x, nextPos.x) + halfW;
-        const minY = Math.min(prevPos.y, nextPos.y) - halfH;
-        const maxY = Math.max(prevPos.y, nextPos.y) + halfH;
+      const prevPos = bullet.position;
+      const nextPos = bullet.nextPosition();
 
-        // WALL COLLISION
-        const nearbyWalls = this.room.grid.getObjectsInArea(
-          minX,
-          maxX,
-          minY,
-          maxY,
-          "wall",
-        );
-        let wallHits = [];
-        for (const wall of nearbyWalls) {
-          const res = sweptSATRectVsRect(
-            prevPos,
-            nextPos,
-            bullet.width,
-            bullet.height,
-            ((bullet.direction - 90) * Math.PI) / 180,
-            wall.position,
-            wall.width,
-            wall.height,
-            wall.angle || 0,
-          );
-          if (res.hit) wallHits.push({ wall, t: res.t });
-        }
-        wallHits.sort((a, b) => a.t - b.t);
+      if (isPositionOutsideMapBounds(this.room, prevPos)) { toRemove.push(id); continue; }
 
-        let currPos = prevPos;
-        let fullVec = new Vec2(nextPos.x - prevPos.x, nextPos.y - prevPos.y);
-        let remainingVec = fullVec;
-        let bulletDestroyed = false;
+      this.room.grid.updateObject(bullet, nextPos);
 
-        if (wallHits.length > 0) {
-          for (const hit of wallHits) {
-            const hitPos = new Vec2(
-              currPos.x + remainingVec.x * hit.t,
-              currPos.y + remainingVec.y * hit.t,
-            );
-            if (bullet.modifiers.size) {
-              for (const mod of bullet.modifiers) {
-                if (mod === "DestroyWalls(DestroySelf)") {
-                  DestroyWall(hit.wall, this.room);
-                  bulletDestroyed = true;
-                  break;
-                }
-                if (mod === "DestroyWalls") DestroyWall(hit.wall, this.room);
-              }
-            } else bulletDestroyed = true;
+      const potentialHits = this.getPotentialHits(bullet, prevPos, nextPos);
 
-            currPos = hitPos;
-            if (bulletDestroyed) break;
-            remainingVec = new Vec2(
-              nextPos.x - currPos.x,
-              nextPos.y - currPos.y,
-            );
-          }
-        }
+      let currPos = prevPos;
+      let remainingVec = nextPos.subtract(currPos);
+      let destroyed = false;
 
-        // PLAYER/BOT COLLISION (SAT)
-        if (!bulletDestroyed) {
-          const nearbyEntities = this.room.grid.getObjectsInArea(minX, maxX, minY, maxY, "player")
-                                 .concat(
-                                 this.room.grid.getObjectsInArea(minX, maxX, minY, maxY, "bot"),
-            );
+      for (const hit of potentialHits) {
+        const hitPos = currPos.add(remainingVec.scale(hit.t));
 
-          for (const obj of nearbyEntities) {
-            if (
-              !obj.alive ||
-              obj === bullet.owner ||
-              (obj.objectType === "player" && this.isAlly(bullet.owner, obj))
-            )
-              continue;
+        if (hit.type === "wall") destroyed = this.handleWallHit(hit.obj, bullet);
+        else if (hit.type === "entity") destroyed = this.handleEntityHit(hit.obj, bullet, currPos);
 
-            const res = sweptSATRectVsRect(
-              currPos,
-              nextPos,
-              bullet.width,
-              bullet.height,
-              ((bullet.direction - 90) * Math.PI) / 180,
-              obj.position,
-              obj.width,
-              obj.height,
-              obj.angle || 0,
-            );
-
-            if (res.hit) {
-              const finalDamage = bullet.damageConfig.length
-                ? calculateFinalDamage(
-                    Vec2.distanceSquared(bullet.startPosition, currPos),
-                    bullet.maxDistance,
-                    bullet.damage,
-                    bullet.damageConfig,
-                  )
-                : bullet.damage;
-
-              switch (obj.objectType) {
-                case "player":
-                  bullet.owner.HandleSelfBulletsOtherPlayerCollision(
-                    obj,
-                    finalDamage,
-                    bullet.gunId,
-                    this.room,
-                  );
-                  break;
-
-                case "bot":
-                  obj.damage(finalDamage, bullet.owner);
-                  break;
-
-                default:
-              }
-
-              if (bullet.afflictionConfig) {
-                const a = bullet.afflictionConfig;
-                this.room.activeAfflictions.push({
-                  shootingPlayer: bullet.owner,
-                  target: obj,
-                  damage: a.damage,
-                  speed: a.waitTime,
-                  gunid: bullet.gunId,
-                  nextTick: Date.now() + a.waitTime,
-                  expires: Date.now() + a.activeTime,
-                });
-              }
-
-              bulletDestroyed = true;
-              break;
-            }
-          }
-        }
-
-        if (bulletDestroyed) {
-          toRemove.push(id);
-        } else {
-          bullet.prevPosition = prevPos;
-          bullet.position = new Vec2(
-            currPos.x + remainingVec.x,
-            currPos.y + remainingVec.y,
-          );
-          bullet.new = false;
-        }
-
-        if (!bullet.new) bullet.effect = 0;
-        // if (bulletDestroyed) bullet.effect = 3
+        currPos = hitPos;
+        if (destroyed) break;
+        remainingVec = nextPos.subtract(currPos);
       }
+
+      if (destroyed) toRemove.push(id);
+      else { bullet.prevPosition = prevPos; bullet.position = currPos.add(remainingVec); bullet.new = false; }
+
+      if (!bullet.new) bullet.effect = 0;
     }
 
-    for (const id of toRemove) this.killBullet(id);
+    toRemove.forEach(id => this.killBullet(id));
+  }
+
+  getPotentialHits(bullet, prevPos, nextPos) {
+    const halfW = bullet.width / 2, halfH = bullet.height / 2;
+    const minX = Math.min(prevPos.x, nextPos.x) - halfW, maxX = Math.max(prevPos.x, nextPos.x) + halfW;
+    const minY = Math.min(prevPos.y, nextPos.y) - halfH, maxY = Math.max(prevPos.y, nextPos.y) + halfH;
+    const hits = [];
+
+    for (const wall of this.room.grid.getObjectsInArea(minX, maxX, minY, maxY, "wall")) {
+      const res = sweptSATRectVsRect(prevPos, nextPos, bullet.width, bullet.height, (bullet.direction - 90) * Math.PI / 180, wall.position, wall.width, wall.height, wall.angle || 0);
+      if (res.hit) hits.push({ obj: wall, t: res.t, type: "wall" });
+    }
+
+    const entities = [...this.room.grid.getObjectsInArea(minX, maxX, minY, maxY, "player"), ...this.room.grid.getObjectsInArea(minX, maxX, minY, maxY, "bot")];
+    for (const entity of entities) {
+      if (!entity.alive || entity === bullet.owner || this.isAlly(bullet.owner, entity)) continue;
+      const res = sweptSATRectVsRect(prevPos, nextPos, bullet.width, bullet.height, (bullet.direction - 90) * Math.PI / 180, entity.position, entity.width, entity.height, entity.angle || 0);
+      if (res.hit) hits.push({ obj: entity, t: res.t, type: "entity" });
+    }
+
+    return hits.sort((a, b) => a.t - b.t);
+  }
+
+  handleWallHit(wall, bullet) {
+    if (bullet.modifiers?.has("DestroyWalls(DestroySelf)")) { DestroyWall(wall, this.room); return true; }
+    if (bullet.modifiers?.has("DestroyWalls")) DestroyWall(wall, this.room);
+    return !bullet.modifiers?.has("DestroyWalls");
+  }
+
+  handleEntityHit(entity, bullet, currPos) {
+    const finalDamage = bullet.damageConfig?.length
+      ? calculateFinalDamage(Vec2.distanceSquared(bullet.startPosition, currPos), bullet.maxDistance, bullet.damage, bullet.damageConfig)
+      : bullet.damage;
+
+    if (entity.objectType === "player") bullet.owner.HandleSelfBulletsOtherPlayerCollision(entity, finalDamage, bullet.gunId, this.room);
+    else if (entity.objectType === "bot") entity.damage(finalDamage, bullet.owner);
+
+    if (bullet.afflictionConfig) {
+      const { damage, waitTime, activeTime, gunId } = bullet.afflictionConfig;
+      this.room.activeAfflictions.push({ shootingPlayer: bullet.owner, target: entity, damage, speed: waitTime, gunid: gunId, nextTick: Date.now() + waitTime, expires: Date.now() + activeTime });
+    }
+
+    return true;
   }
 
   killBullet(bulletId) {
@@ -288,56 +177,32 @@ class BulletManager {
   }
 
   isAlly(owner, other) {
-    if (!owner || !other) return false;
-    if (owner.id === other.id) return false;
-    if (!this.room.IsTeamMode) return false;
-    return owner.team.id === other.team.id;
+    return owner?.id !== other?.id && this.room.IsTeamMode && owner?.team.id === other?.team.id;
   }
 
   processScheduledBullets() {
     const now = Date.now();
-    for (let i = this.scheduledBullets.length - 1; i >= 0; i--) {
-      const s = this.scheduledBullets[i];
-      if (now >= s.spawnTime) {
-        if (s.owner) this.spawnBullet(s.owner, s.bulletData);
-        this.scheduledBullets.splice(i, 1);
-      }
-    }
+    this.scheduledBullets = this.scheduledBullets.filter(s => {
+      if (now >= s.spawnTime) this.spawnBullet(s.owner, s.bulletData);
+      else return true;
+    });
   }
 
   scheduleBullet(player, bulletData, delayMs) {
-    this.scheduledBullets.push({
-      spawnTime: Date.now() + delayMs,
-      owner: player,
-      bulletData,
-    });
+    this.scheduledBullets.push({ spawnTime: Date.now() + delayMs, owner: player, bulletData });
   }
 }
 
-
 function DestroyWall(wall, room) {
   room.grid.removeObject(wall);
-  AddNewUnseenObject(room, {
-    objectType: "static_obj",
-    id: wall.gid,
-    position: wall.position,
-    sendx: wall.position.x,
-    sendy: wall.position.y,
-  });
+  AddNewUnseenObject(room, { objectType: "static_obj", id: wall.gid, position: wall.position, sendx: wall.position.x, sendy: wall.position.y });
 }
 
-function calculateFinalDamage(
-  distanceSquaredUsed,
-  bulletMaxDistance,
-  normalDamage,
-  layers,
-) {
-  if (!layers.length) return normalDamage;
-  const maxDistSq = bulletMaxDistance * bulletMaxDistance;
+function calculateFinalDamage(distanceSq, maxDistance, baseDamage, layers) {
+  if (!layers.length) return baseDamage;
+  const maxDistSq = maxDistance * maxDistance;
   for (const layer of layers) {
-    const thresholdDistanceSq = (layer.threshold / 100) * maxDistSq;
-    if (distanceSquaredUsed <= thresholdDistanceSq)
-      return Math.ceil(normalDamage * layer.damageMultiplier);
+    if (distanceSq <= (layer.threshold / 100) * maxDistSq) return Math.ceil(baseDamage * layer.damageMultiplier);
   }
   return 0;
 }
@@ -345,39 +210,28 @@ function calculateFinalDamage(
 function handleBulletFired(room, player, gunType) {
   const gun = gunsconfig[gunType];
   const now = Date.now();
-  if (player.shooting || now - (player.lastShootTime || 0) < gun.cooldown)
-    return;
+  if (player.shooting || now - (player.lastShootTime || 0) < gun.cooldown) return;
+
   player.shooting = true;
   player.lastShootTime = now;
 
-  for (const bulletConfig of gun.bullets) {
-    const bullet_tick_rate = GlobalRoomConfig.ticks_per_second /// 3;
+  const bullet_tick_rate = GlobalRoomConfig.ticks_per_second / 3;
 
-    const bulletdata = {
-      ...bulletConfig,
+  gun.bullets.forEach(bulletConfig => {
+    const bulletData = {
       ...gun,
+      ...bulletConfig,
       client_render_speed: Math.round(bulletConfig.speed),
       speed: bulletConfig.speed * (GlobalRoomConfig.ticks_per_second / bullet_tick_rate),
       updates_per_tick: bullet_tick_rate,
- 
-      angle: bulletConfig.usePlayerAngle
-        ? player.shoot_direction + bulletConfig.angle
-        : bulletConfig.angle,
+      angle: bulletConfig.usePlayerAngle ? player.shoot_direction + bulletConfig.angle : bulletConfig.angle,
       maxTime: Date.now() + gun.maxexistingtime + bulletConfig.delay,
       gunId: gunType,
     };
+    room.bulletManager.scheduleBullet(player, bulletData, bulletConfig.delay);
+  });
 
-    room.bulletManager.scheduleBullet(player, bulletdata, bulletConfig.delay);
-  }
-
-  room.setRoomTimeout(() => {
-    player.shooting = false;
-  }, gun.cooldown);
+  room.setRoomTimeout(() => { player.shooting = false; }, gun.cooldown);
 }
 
-module.exports = {
-  BulletManager,
-  handleBulletFired,
-  Vec2,
-  sweptSATRectVsRect,
-};
+module.exports = { BulletManager, handleBulletFired, Vec2, sweptSATRectVsRect };
--- src/utils/bulletcollision.js
+++ src/utils/bulletcollision.js
@@ -16,6 +16,11 @@ class Vec2 {
     return new Vec2(this.x + v.x, this.y + v.y);
   }
 
+  subtract(vec) {
+    return new Vec2(this.x - vec.x, this.y - vec.y);
+  }
+
+
   scale(s) {
     return new Vec2(this.x * s, this.y * s);
   }
@@ -25,6 +30,125 @@ class Vec2 {
     const dy = a.y - b.y;
     return dx * dx + dy * dy;
   }
+
+   distance(vec) {
+    return Math.sqrt(this.distanceSquared(vec));
+  }
+}
+
+function sweptSATRectVsRectOnlyFrontPoints(start, end, w, h, angle, targetCenter, targetW, targetH, targetAngle) {
+  // For tiny bullets, fallback to AABB
+  if (w < 3 && h < 3) {
+    const minX = Math.min(start.x, end.x);
+    const maxX = Math.max(start.x, end.x);
+    const minY = Math.min(start.y, end.y);
+    const maxY = Math.max(start.y, end.y);
+
+    const tMinX = targetCenter.x - targetW / 2;
+    const tMaxX = targetCenter.x + targetW / 2;
+    const tMinY = targetCenter.y - targetH / 2;
+    const tMaxY = targetCenter.y + targetH / 2;
+
+    return maxX < tMinX || minX > tMaxX || maxY < tMinY || minY > tMaxY
+      ? { hit: false }
+      : { hit: true, t: 0, tExit: 1 };
+  }
+
+  const rad = angle;
+  const cos = Math.cos(rad);
+  const sin = Math.sin(rad);
+
+  // Calculate only the two front points of the bullet
+  const frontOffsetX = w / 2;
+  const frontOffsetY = h / 2;
+
+  const startFront1 = {
+    x: start.x + cos * frontOffsetX - sin * -frontOffsetY,
+    y: start.y + sin * frontOffsetX + cos * -frontOffsetY
+  };
+  const startFront2 = {
+    x: start.x + cos * frontOffsetX - sin * frontOffsetY,
+    y: start.y + sin * frontOffsetX + cos * frontOffsetY
+  };
+
+  const endFront1 = {
+    x: end.x + cos * frontOffsetX - sin * -frontOffsetY,
+    y: end.y + sin * frontOffsetX + cos * -frontOffsetY
+  };
+  const endFront2 = {
+    x: end.x + cos * frontOffsetX - sin * frontOffsetY,
+    y: end.y + sin * frontOffsetX + cos * frontOffsetY
+  };
+
+  const bulletPointsStart = [startFront1, startFront2];
+  const bulletPointsEnd = [endFront1, endFront2];
+
+  // Rotate target corners
+  function rotatePoint(p, angleRad) {
+    const c = Math.cos(angleRad), s = Math.sin(angleRad);
+    return { x: c * p.x - s * p.y, y: s * p.x + c * p.y };
+  }
+
+  function getCorners(center, w, h, angle) {
+    const hw = w / 2, hh = h / 2;
+    const local = [
+      { x: -hw, y: -hh }, { x: hw, y: -hh },
+      { x: hw, y: hh }, { x: -hw, y: hh }
+    ];
+    return local.map(p => {
+      const r = rotatePoint(p, angle);
+      return { x: r.x + center.x, y: r.y + center.y };
+    });
+  }
+
+  const targetCorners = getCorners(targetCenter, targetW, targetH, targetAngle);
+
+  // SAT axes: only edges of target + bullet front line
+  const axes = [];
+  // Edge of bullet front
+  const edge = { x: bulletPointsStart[1].x - bulletPointsStart[0].x, y: bulletPointsStart[1].y - bulletPointsStart[0].y };
+  const len = Math.hypot(edge.x, edge.y);
+  axes.push({ x: -edge.y / len, y: edge.x / len });
+
+  // Edges of target rectangle
+  for (let i = 0; i < 4; i++) {
+    const p1 = targetCorners[i], p2 = targetCorners[(i + 1) % 4];
+    const e = { x: p2.x - p1.x, y: p2.y - p1.y };
+    const l = Math.hypot(e.x, e.y);
+    axes.push({ x: -e.y / l, y: e.x / l });
+  }
+
+  function project(points, axis) {
+    let min = Infinity, max = -Infinity;
+    for (const p of points) {
+      const proj = p.x * axis.x + p.y * axis.y;
+      if (proj < min) min = proj;
+      if (proj > max) max = proj;
+    }
+    return { min, max };
+  }
+
+  let tEnter = 0, tExit = 1;
+
+  for (const axis of axes) {
+    const projStart = project(bulletPointsStart, axis);
+    const projEnd = project(bulletPointsEnd, axis);
+    const projTarget = project(targetCorners, axis);
+
+    const vel = ((projEnd.min + projEnd.max) - (projStart.min + projStart.max)) / 2;
+
+    if (vel === 0) {
+      if (projStart.max < projTarget.min || projStart.min > projTarget.max) return { hit: false };
+    } else {
+      const t0 = (projTarget.min - projStart.max) / vel;
+      const t1 = (projTarget.max - projStart.min) / vel;
+      tEnter = Math.max(tEnter, Math.min(t0, t1));
+      tExit = Math.min(tExit, Math.max(t0, t1));
+      if (tEnter > tExit) return { hit: false };
+    }
+  }
+
+  return tEnter >= 0 && tEnter <= 1 ? { hit: true, t: tEnter, tExit } : { hit: false };
 }
 
 /* =========================
